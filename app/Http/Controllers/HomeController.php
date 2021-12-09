0e957acf17ee65d4395fec8a0fc35baac53b4c78
hsoubcontrolpanel <hsoubcontrolpanel@gmail.com>
2021-12-09 21:01:58 +0200

    add db

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Bid;
-use App\Models\product;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
