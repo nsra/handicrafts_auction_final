0b9f47ee7c1f1cb2d8e674d9a052b598c2944f93
hsoubcontrolpanel <hsoubcontrolpanel@gmail.com>
2021-12-13 06:11:12 +0200

    for deploy

--- config/mail.php
+++ config/mail.php
@@ -59,7 +59,7 @@ return [
 
         'sendmail' => [
             'transport' => 'sendmail',
-            'path' => '/usr/sbin/sendmail -bs',
+            'sendmail' => '/your/sendmail/path -t',
         ],
 
         'log' => [
